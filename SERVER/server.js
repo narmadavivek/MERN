a79001a9dc83362acba57b1cb9a9d00dbb8891a8
narmadavivek <nandyvivek98@gmail.com>
2024-03-27 14:59:46 +0530

    render

--- SERVER/server.js
+++ SERVER/server.js
@@ -11,7 +11,7 @@ const adminRoute = require("./router/admin-router");
 
 //let's tackle cors
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: true ,
   methods:"GET, POST, PUT, DELETE, PATCH, HEAD",
   credentials: true,
 };
